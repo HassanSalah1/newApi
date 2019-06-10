f98d31a150f8e5bec5551cc382000bc0fcf0c110
Hassan Salah <hs8266@gmail.com>
2019-06-10 18:02:33 +0200

    Add Api Resources

--- app/Http/Resources/Porduct/ProductResource.php
+++ app/Http/Resources/Porduct/ProductResource.php
@@ -14,6 +14,20 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->title,
+            'description' => $this->detail,
+            'price' => $this->price,
+            'stock' => $this->stock == 0 ? "Out Of Stock" : $this->stock ,
+            'discount' => $this->discount,
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ?
+                round($this->reviews->sum('star') / $this->reviews->count(),2)
+                : 'No Rating Yet'
+            ,
+            'href' =>[
+                'reviews' => route('reviews.index', $this->id)
+            ]
+        ];
     }
 }
