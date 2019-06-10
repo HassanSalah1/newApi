54725d0a04909a126af82414dcc5857baf1da1cd
Hassan Salah <hs8266@gmail.com>
2019-06-10 17:11:48 +0200

    Intial Resources

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(50, 1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2, 30),
     ];
 });
